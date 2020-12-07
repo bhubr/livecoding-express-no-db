ebc4a7feb1f79e57d033041b39bc769c03859112
Benoit Hubert <benoithubert@gmail.com>
2020-12-07 14:47:05 +0100

    Add GET routes

--- server.js
+++ server.js
@@ -4,7 +4,30 @@ const port = 5000;
 
 const app = express();
 
-// more to come
+// Routes
+
+app.get('/', (req, res) => {
+  res
+    // .set('content-type', 'text/plain')
+    .end('<h1>hello</h1>');
+});
+
+const users = [
+  { id: 1, name: 'John Lennon' },
+  { id: 2, name: 'Ringo Starr' }
+];
+
+// Récupérer une liste d'utilisateurs
+app.get('/users', (req, res) => {
+  res.send(users);
+});
+
+// Récupérer un utilisateur spécifique
+app.get('/users/:id', (req, res) => {
+  const userId = Number(req.params.id);
+  const user = users.find(item => item.id === userId);
+  res.send(user);
+});
 
 app.listen(port, (err) => {
   if (err) {
